21039b3fba56238ed09ea6bfe7448ee6d74f07df
aneesa04 <aneesafatima238@gmail.com>
2024-03-27 00:25:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+
+
+function App() {
+ 
+
+  return (
+    <div className="bg-[#497285] h-screen">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="bg-[#2b4450] h-[8vh]">
+      Hangman Game
+    </div>
+  )
+}
+
+export default Header;
